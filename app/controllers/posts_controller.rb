c33c46d31838ca23d14aa195afe3b9f37634411a
Seth Jones <sethjmusic@gmail.com>
2015-07-26 13:39:44 -0400

    edit and update: posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,18 @@ class PostsController < ApplicationController
       end
   end
 
-  def edit
-  end
+   def edit
+     @post = Post.find(params[:id])
+   end
+
+   def update
+     @post = Post.find(params[:id])
+     if @post.update_attributes(params.require(:post).permit(:title, :body))
+       flash[:notice] = "Post was updated."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+   end
 end
