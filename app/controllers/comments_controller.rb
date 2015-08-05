533b066fa89571cf2defdda64d4a37a05d581f91
Seth Jones <sethjmusic@gmail.com>
2015-08-05 15:52:56 -0400

    modified nesting routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,8 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @topic = Topic.find( params[:topic_id] )
-    @post = @topic.posts.find( params[:post_id] )
+
+    @post = Post.find( params[:post_id] )
     @comments = @post.comments
 
     @comment = Comment.new(comment_params)
@@ -12,25 +12,25 @@ class CommentsController < ApplicationController
     @comment.user = current_user
 
     if @comment.save
-      redirect_to [@topic, @post], notice: "Comment saved successfully"
+      redirect_to [@post.topic, @post], notice: "Comment saved successfully"
     else
-      redirect_to [@topic, @post], notice: "There was an error saving the comment. Please try again."
+      redirect_to [@post.topic, @post], notice: "There was an error saving the comment. Please try again."
     end
     authorize @comment
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was deleted."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error deleting the comment. Please try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
