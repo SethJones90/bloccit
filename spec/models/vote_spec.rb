d25da3d5e31558df8b43ef18a07209c29032605c
Seth Jones <sethjmusic@gmail.com>
2015-08-05 21:28:54 -0400

    added vote spec

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        expect ( @vote.value ).to eq(1).or eq(-1)
+      end
+    end
+  end
+end
